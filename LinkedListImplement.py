ce2194ba155293faba52974dc56767d5e1f1f0bd
abrehan2 <rehannajam2@gmail.com>
2023-09-04 17:43:54 +0500

    initial commit

---
+++ LinkedListImplement.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data: None, next: None):
+        self.data = data
+        self.next = next
+
+class Linked_list:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_begining(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+
+        itr = self.head
+        listStr = ""
+
+        while itr:
+            listStr += str(itr.data) + "-->"
+            itr = itr.next
+
+        print(listStr)
+
+if __name__ == "__main__":
+   linked_list_object = Linked_list()
+   linked_list_object.insert_at_begining(1)
+   linked_list_object.insert_at_begining(2)
+   linked_list_object.insert_at_begining(3)
+   linked_list_object.print()
