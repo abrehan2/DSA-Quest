cb29b50f4c7a7a46bf1cc65adff5949cc1b80f3d
abrehan2 <rehannajam2@gmail.com>
2023-11-19 00:09:37 +0500

    Made a few changes

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,7 +1,7 @@
 def insertion_sort(elem):
     for i in range(1, len(elem)):
         anchor=elem[i]
-        j = j - 1
+        j = i - 1
         while j >=0 and anchor < elem[j]:
             elem[j+1] = elem[j]
             j = j - 1
